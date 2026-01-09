167f9a79fc1d6e78a01fab33272bab89018abed6
Peterjah <ps@massa.net>
2026-01-09 13:27:27 +0100

    display linked mns

--- src/pages/settings/ShareContactPage.tsx
+++ src/pages/settings/ShareContactPage.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import ShareContact from '../../components/settings/ShareContact';
 import { useAccountStore } from '../../stores/accountStore';
+import { useUserMnsDomain } from '../../hooks/useUserMnsDomain';
 import { ROUTES } from '../../constants/routes';
 
 const ShareContactPage: React.FC = () => {
   const navigate = useNavigate();
   const { userProfile, session } = useAccountStore();
+  const { mnsDomains } = useUserMnsDomain();
+  const mnsDomain = mnsDomains.length > 0 ? mnsDomains[0] : null;
 
   const handleBack = () => {
     navigate(-1);
@@ -24,6 +27,7 @@ const ShareContactPage: React.FC = () => {
       userId={userProfile.userId}
       userName={userProfile.username}
       publicKey={session.ourPk}
+      mnsDomain={mnsDomain}
     />
   );
 };
