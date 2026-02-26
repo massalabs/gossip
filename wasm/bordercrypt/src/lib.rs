34e7ea2e40831b77af3865e5e5b478a50aa48340
ben888 <br@massa.net>
2026-02-27 00:55:42 +0700

    feat(bordercrypt): add keypair file serialization
    Binary format: [version: u32 BE] [pq_pk] [sk_nonce: 16B] [sk_ct: remaining]
    with serialize/deserialize and storage helpers.

--- wasm/bordercrypt/src/lib.rs
+++ wasm/bordercrypt/src/lib.rs
@@ -2,14 +2,17 @@
 //!
 //! Spec: <https://github.com/massalabs/gossip/discussions/380>
 
+mod block;
 mod constants;
 mod domain;
 mod error;
 mod kdf;
+mod keypair;
 mod pq;
 pub mod storage;
 mod types;
 
+pub use block::{create_cover_block, decrypt_block, encrypt_block, rerandomize_block};
 pub use constants::{AEAD_TAG_SIZE, BLOCK_SIZE, LENGTH_HDR_SIZE, PLAINTEXT_SIZE, SESSION_COUNT};
 pub use domain::{
     block_aead_aad, block_aead_key_label, block_kdf_salt, block_scope, password_kdf_salt, root,
@@ -17,6 +20,7 @@ pub use domain::{
 };
 pub use error::{BordercryptError, Result};
 pub use kdf::derive_block_aead_key;
+pub use keypair::{KeypairFile, read_session_keypair, read_session_version_and_pk};
 pub use pq::{
     PQ_CT_SIZE, PQ_MSG_SIZE, PqPublicKey, PqSecretKey, pq_decrypt, pq_encrypt, pq_keygen, pq_rerand,
 };
